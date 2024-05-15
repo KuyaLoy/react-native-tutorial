1cf30e81c8d174abcf76a93014f2fa040eb5b436
Robin Tapiru <robintapiru0894@gmail.com>
2024-05-15 17:02:18 +0400

    deeplink

--- styler02/App.tsx
+++ styler02/App.tsx
@@ -3,12 +3,15 @@ import React from 'react';
 import {ScrollView, Text, SafeAreaView} from 'react-native';
 import FlatCards from './components/FlatCards';
 import ElevatedCard from './components/ElevatedCard';
+import FancyCard from './components/FancyCard';
+
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
         <FlatCards />
         <ElevatedCard />
+        <FancyCard />
       </ScrollView>
     </SafeAreaView>
   );
---
+++ styler02/components/FancyCard.tsx
@@ -0,0 +1,79 @@
+import {StyleSheet, Text, View, Image} from 'react-native';
+export default function FancyCard() {
+  return (
+    <View>
+      <Text style={styles.headingText}>Trending Places</Text>
+      <View style={[styles.card, styles.cardElevated]}>
+        <Image
+          source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+          style={styles.cardImage}
+        />
+        <View style={styles.cardBody}>
+          <Text style={styles.cardTitle}>React Native</Text>
+          <Text style={styles.cardLabel}>Robin Tapiru</Text>
+          <Text style={styles.cardDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+            veritatis sed qui esse. Tempore sunt autem odit similique, sequi
+            modi quo aliquam totam eum nesciunt sint aperiam expedita et velit.
+          </Text>
+          <Text style={styles.cardFooter}>Aug 10, 1994</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  card: {
+    width: 350,
+    height: 360,
+    borderRadius: 6,
+    marginVertical: 12,
+    marginHorizontal: 16,
+  },
+  cardElevated: {
+    backgroundColor: '#fff',
+    elevation: 3,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
+  cardImage: {
+    height: 180,
+    marginBottom: 8,
+    borderTopLeftRadius: 6,
+    borderTopRightRadius: 6,
+  },
+  cardBody: {
+    flex: 1,
+    flexGrow: 1,
+    paddingHorizontal: 12,
+  },
+
+  cardTitle: {
+    color: '#000',
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 4,
+  },
+  cardLabel: {
+    color: '#000',
+    fontSize: 14,
+    marginBottom: 6,
+  },
+  cardDescription: {
+    color: '#242b2e',
+    fontSize: 12,
+    marginBottom: 12,
+    marginTop: 6,
+    flexShrink: 1,
+  },
+  cardFooter: {
+    color: '#000',
+  },
+});
